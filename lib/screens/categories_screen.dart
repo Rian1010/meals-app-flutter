98fae9362a600e55dee3ced2d8628c5a61e4f01a
Rian <rian@ously.de>
2021-11-24 15:41:40 +0100

    Trying to solve a bug

--- lib/screens/categories_screen.dart
+++ lib/screens/categories_screen.dart
@@ -9,27 +9,22 @@ class CategoriesScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     // use GridView.builder for a dynamic amount of items
     // Slivers are sscrollable areas on the screen
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('DeliMeal'),
-      ),
-      body: GridView(
-        padding: const EdgeInsets.all(25),
-        children: DUMMY_CATEGORIES
-            .map(
-              (catData) => CategoryItem(
-                catData.id,
-                catData.title,
-                catData.color,
-              ),
-            )
-            .toList(),
-        gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
-          maxCrossAxisExtent: 200,
-          childAspectRatio: 3 / 2,
-          crossAxisSpacing: 20,
-          mainAxisSpacing: 20,
-        ),
+    return GridView(
+      padding: const EdgeInsets.all(25),
+      children: DUMMY_CATEGORIES
+          .map(
+            (catData) => CategoryItem(
+              catData.id,
+              catData.title,
+              catData.color,
+            ),
+          )
+          .toList(),
+      gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+        maxCrossAxisExtent: 200,
+        childAspectRatio: 3 / 2,
+        crossAxisSpacing: 20,
+        mainAxisSpacing: 20,
       ),
     );
   }
---
+++ lib/screens/tabs_screen.dart
@@ -0,0 +1,94 @@
+import 'package:flutter/material.dart';
+import 'package:meals_app/screens/categories_screen.dart';
+import 'package:meals_app/screens/favourites_screen.dart';
+
+class TabsScreen extends StatefulWidget {
+  const TabsScreen({Key? key}) : super(key: key);
+
+  @override
+  _TabsScreenState createState() => _TabsScreenState();
+}
+
+class _TabsScreenState extends State<TabsScreen> {
+  final _pages = [
+    {
+      'page': CategoriesScreen(),
+      'title': 'Categories',
+    },
+    {
+      'page': FavoritesScreen(),
+      'title': 'Your Favorites',
+    },
+  ];
+
+  int _selectPageIndex = 0;
+
+  void _selectPage(int idx) {
+    setState(() {
+      _selectPageIndex = idx;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(_pages[_selectPageIndex]['title'] as String),
+      ),
+      body: _pages[_selectPageIndex]['page'] as Widget,
+      bottomNavigationBar: BottomNavigationBar(
+        onTap: _selectPage,
+        backgroundColor: Theme.of(context).primaryColor,
+        unselectedItemColor: Colors.white,
+        selectedItemColor: Theme.of(context).accentColor,
+        currentIndex: _selectPageIndex,
+        // type: BottomNavigationBarType.shifting,
+        items: [
+          BottomNavigationBarItem(
+            // backgroundColor: Theme.of(context).primaryColor,
+            icon: Icon(Icons.category),
+            label: 'Categories',
+          ),
+          BottomNavigationBarItem(
+            // backgroundColor: Theme.of(context).primaryColor,
+            icon: Icon(Icons.star),
+            label: 'Favorites',
+          ),
+        ],
+      ),
+    );
+    // return DefaultTabController(
+    //   length: 2,
+    //   initialIndex: 0,
+    //   child: Scaffold(
+    //     appBar: AppBar(
+    //       title: Text('Meals'),
+    //       bottom: TabBar(
+    //         tabs: <Widget>[
+    //           Tab(
+    //             icon: Icon(
+    //               Icons.category,
+    //             ),
+    //             text: 'Categories',
+    //           ),
+    //           Tab(
+    //             icon: Icon(
+    //               Icons.star,
+    //             ),
+    //             text: 'Favorites',
+    //           ),
+    //         ],
+    //       ),
+    //     ),
+    //     body: TabBarView(
+    //       children: <Widget>[
+    //         // 1st tab automatically activates CategoriesScreen()
+    //         CategoriesScreen(),
+    //         // 2nd tab automatically activates FavoritesScreen()
+    //         FavoritesScreen(),
+    //       ],
+    //     ),
+    //   ),
+    // );
+  }
+}
